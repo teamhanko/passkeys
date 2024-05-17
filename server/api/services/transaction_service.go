1d0bd9212ed9551250a6074878703752b1306017
Stefan Jacobi <sjacobi@mateki.de>
2024-05-17 11:57:35 +0200

    fix(transaction): add length check
    add if transaction slice is bigger than 0
    
    Co-authored-by: Frederic Jahn <frederic.jahn@hanko.io>

--- server/api/services/transaction_service.go
+++ server/api/services/transaction_service.go
@@ -68,7 +68,7 @@ func (ts *transactionService) Initialize(userId string, transaction *models.Tran
 		return nil, echo.NewHTTPError(http.StatusInternalServerError, "unable to search for transaction")
 	}
 
-	if foundTransaction != nil {
+	if foundTransaction != nil && len(*foundTransaction) > 0 {
 		ts.logger.Error("transaction already exists")
 		return nil, echo.NewHTTPError(http.StatusConflict, "transaction already exists")
 	}
